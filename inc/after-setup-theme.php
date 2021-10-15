c7c14558bc335919e84be730f5c276dc834b18db
Steve Ryan <steve.ryan@asu.edu>
2021-10-15 16:30:33 -0700

    Add theme support for various options. Add footer menus.

--- inc/after-setup-theme.php
+++ inc/after-setup-theme.php
@@ -5,19 +5,39 @@
  * @package pitchfork
  */
 
-add_action( 'after_setup_theme', 'uds_pitchfork_setup' );
-if ( ! function_exists( 'uds_pitchfork_setup' ) ) {
+add_action( 'after_setup_theme', 'pitchfork_theme_setup' );
+if ( ! function_exists( 'pitchfork_theme_setup' ) ) {
 	
 	/**
 	 * Setup functions for theme.
 	 *
 	 * @return void
 	 */
-	function uds_pitchfork_setup() {
+	function pitchfork_theme_setup() {
+		// Add default posts and comments RSS feed links to head.
 		add_theme_support( 'automatic-feed-links' );
 
-		add_theme_support( 'customize-selective-refresh-widgets' );
+		/*
+		 * Let WordPress manage the document title.
+		 * By adding theme support, we declare that this theme does not use a
+		 * hard-coded <title> tag in the document head, and expect WordPress to
+		 * provide it for us.
+		 */
+		add_theme_support( 'title-tag' );
 
+		// Register nav menus.
+		register_nav_menus(
+			array(
+				'primary' => __( 'Main Menu', 'uds-wordpress-theme' ),
+				'footer' => __( 'Footer Menu', 'uds-wordpress-theme' ),
+				'social-media' => __( 'Social Media Menu', 'uds-wordpress-theme' ),
+			)
+		);
+
+		/*
+		 * Switch default core markup for search form, comment form, and comments
+		 * to output valid HTML5.
+		 */
 		add_theme_support(
 			'html5',
 			array(
@@ -26,21 +46,20 @@ if ( ! function_exists( 'uds_pitchfork_setup' ) ) {
 				'comment-list',
 				'gallery',
 				'caption',
-				'style',
 				'script',
-				'navigation-widgets',
+				'style',
 			)
 		);
 
-		add_theme_support( 'post-thumbnails' );
+		/*
+		 * Adding Thumbnail basic support
+		 */
+		add_theme_support( 'post-thumbnails', array( 'post' ) );
 
-		add_theme_support( 'title-tag' );
-
-		register_nav_menus(
-			array(
-				'primary' => esc_html__( 'Primary', 'pitchfork' ),
-			)
-		);
+		/*
+		 * Adding support for Widget edit icons in customizer
+		 */
+		add_theme_support( 'customize-selective-refresh-widgets' );
 		
 	}
 }
--- inc/wp-custom-menu.php
+++ inc/wp-custom-menu.php
@@ -20,9 +20,15 @@ if ( ! function_exists( 'uds_wp_get_menu_array' ) ) {
 		$locations = get_nav_menu_locations();
 		if ( isset( $locations[ $menu_name ] ) ) {
 			$menu_object = wp_get_nav_menu_object( $locations[ $menu_name ] );
-			$array_menu  = wp_get_nav_menu_items( $menu_object->term_id );
 
-			// array_menu will return false if there are no menu options.
+			// Prevent php notice if there is no menu assigned to the location.
+			if (is_object($menu_object)) {
+				$array_menu  = wp_get_nav_menu_items( $menu_object->term_id );
+			} else {
+				$array_menu = array();
+			}
+			
+			// array_menu will also return false if there are no menu options within the assigned menu.
 			if ( ! $array_menu ) {
 				$array_menu = array();
 			}
