732ddf68d38ef34b9587897c96461c2116988945
Steve Ryan <steve.ryan@asu.edu>
2022-06-14 09:48:24 -0700

    Remove category label from archive title by default.

--- inc/template-functions.php
+++ inc/template-functions.php
@@ -16,3 +16,29 @@ function pitchfork_pingback_header() {
 	}
 }
 add_action( 'wp_head', 'pitchfork_pingback_header' );
+
+
+if ( ! function_exists( 'pitchfork_custom_archive_title' ) ) {
+	/**
+	 * Remove the default WordPress object label from archive title pages.
+	 * https://developer.wordpress.org/reference/hooks/get_the_archive_title/#user-contributed-notes
+	 *
+	 * @param string $title archive title.
+	 * @return string
+	 */
+	function pitchfork_custom_archive_title( $title ) {
+		if ( is_category() ) {
+			$title = single_cat_title( '', false );
+		} elseif ( is_tag() ) {
+			$title = single_tag_title( '', false );
+		} elseif ( is_author() ) {
+			$title = '<span class="vcard">' . get_the_author() . '</span>';
+		} elseif ( is_tax() ) { // for custom post types.
+			$title = sprintf( '%1$s', single_term_title( '', false ) );
+		} elseif ( is_post_type_archive() ) {
+			$title = post_type_archive_title( '', false );
+		}
+		return $title;
+	}
+}
+add_filter( 'get_the_archive_title', 'pitchfork_custom_archive_title' );
