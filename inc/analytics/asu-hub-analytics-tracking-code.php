58b67d9d80e3e157164ff0b9eccafb0baed3a6fa
Steve Ryan <steve.ryan@asu.edu>
2022-09-12 14:05:14 -0700

    Fix: Remove Lavidge tracking code.
    Code is known to produce odd results based on how the script is included in the theme (not enqueued normally.)

--- inc/analytics/asu-hub-analytics-tracking-code.php
+++ inc/analytics/asu-hub-analytics-tracking-code.php
@@ -30,55 +30,5 @@
 </script>
 <!-- End Google Tag Manager ASU Universal -->
 
-<!-- Lavidge AdWords Tracking -->
-<script>
-	var ssoval;
-	if (document.cookie.indexOf("SSONAME") === -1) {
-		ssoval = "false";
-	} else {
-		ssoval = "true";
-	}
-	var versaTag = {};
-	versaTag.id = "5381";
-	versaTag.sync = 0;
-	versaTag.dispType = "js";
-	versaTag.ptcl = "HTTPS";
-	versaTag.bsUrl = "bs.serving-sys.com/BurstingPipe";
-	//VersaTag activity parameters include all conversion parameters including custom parameters and Predefined parameters. Syntax: "ParamName1":"ParamValue1", "ParamName2":"ParamValue2". ParamValue can be empty.
-	versaTag.activityParams = {
-		//Predefined parameters:
-		"OrderID": "",
-		"Session": "",
-		"Value": "",
-		"productid": "",
-		"productinfo": "",
-		"Quantity": ""
-		//Custom parameters:
-	};
-	//Static retargeting tags parameters. Syntax: "TagID1":"ParamValue1", "TagID2":"ParamValue2". ParamValue can be empty.
-	versaTag.retargetParams = {};
-	//Dynamic retargeting tags parameters. Syntax: "TagID1":"ParamValue1", "TagID2":"ParamValue2". ParamValue can be empty.
-	versaTag.dynamicRetargetParams = {};
-	// Third party tags conditional parameters and mapping rule parameters. Syntax: "CondParam1":"ParamValue1", "CondParam2":"ParamValue2". ParamValue can be empty.
-	versaTag.conditionalParams = {
-		"ssoval": ssoval
-	};
-	//versaTag.conditionalParams = {};
-	//versaTag.conditionalParams["ssoval"] = ssoval;
-</script>
-<script id="ebOneTagUrlId" src="https://secure-ds.serving-sys.com/SemiCachedScripts/ebOneTag.js"></script>
-<noscript>
-	<iframe src="
-    ?
-    cn=ot&amp;
-    onetagid=5381&amp;
-    ns=1&amp;
-    activityValues=$$Value=[Value]&amp;OrderID=[OrderID]&amp;Session=[Session]&amp;ProductID=[ProductID]&amp;ProductInfo=[ProductInfo]&amp;Quantity=[Quantity]$$&amp;
-    retargetingValues=$$$$&amp;
-    dynamicRetargetingValues=$$$$&amp;
-    acp=$$ssoval-false&amp;" style="display:none;width:0px;height:0px"></iframe>
-</noscript>
-<!-- End Lavidge AdWords Tracking -->
-
 <?php
 // @codingStandardsIgnoreEnd
