b8818d4ebac96f317c5f3dd7156af42f1d6c486f
Steve Ryan <steve.ryan@asu.edu>
2022-11-21 16:16:52 -0700

    Remove widgets-init.php from call stack.

--- functions.php
+++ functions.php
@@ -22,17 +22,19 @@ require get_template_directory() . '/inc/header-additions.php';
 require get_template_directory() . '/inc/class-bootstrap-walker-nav-menu.php';
 require get_template_directory() . '/inc/class-wp-social-media-walker.php';
 
+// Theme options page includes controls for the theme's multisite behavior and analytics.
+require get_template_directory() . '/inc/theme-options.php';
+
 // Customizer options.
-require get_template_directory() . '/inc/customizer/customizer-sanitizers.php';            
-require get_template_directory() . '/inc/customizer/customizer-preview-js.php';            
-require get_template_directory() . '/inc/customizer/customizer-controls-js.php';           
-require get_template_directory() . '/inc/customizer/customizer-post-message-support.php'; 
-require get_template_directory() . '/inc/customizer/customizer-endorsed-unit-logos.php'; 
+require get_template_directory() . '/inc/customizer/customizer-sanitizers.php';
+require get_template_directory() . '/inc/customizer/customizer-preview-js.php';
+require get_template_directory() . '/inc/customizer/customizer-controls-js.php';
+require get_template_directory() . '/inc/customizer/customizer-post-message-support.php';
+require get_template_directory() . '/inc/customizer/customizer-endorsed-unit-logos.php';
 require get_template_directory() . '/inc/customizer/customizer-settings.php';
 
 // Render partials from customizer controls. Used for header.php and for selective refresh of the customizer.
 require get_template_directory() . '/inc/render-partials.php';
-
 
 // Add preload for CDN.
 // require get_template_directory() . '/inc/resource-hints.php'; !
@@ -42,9 +44,6 @@ require get_template_directory() . '/inc/template-functions.php';
 
 // Custom template tags for this theme.
 require get_template_directory() . '/inc/template-tags.php';
-
-// Register widget area.
-require get_template_directory() . '/inc/widgets-init.php';
 
 // Load ASU WordPress nav walker.
 require get_template_directory() . '/inc/wp-custom-menu.php';
