e809204c7c19d03996297277b5ebabaae2158ad8
Steve Ryan <steve.ryan@asu.edu>
2023-01-25 13:59:47 -0700

    Adding 3-card across patterns to library.

---
+++ patterns/cards-2-three-across-background.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Title: Cards - Background + three across
+ * Slug: pitchfork/cards-three-across-background
+ * Categories: pitchfork-cards
+ * Block Types: core
+ */
+?>
+
+
+<!-- wp:acf/background-section {"name":"acf/background-section","data":{"uds_background_section_choice":"color","_uds_background_section_choice":"field_603819ea114a9","uds_background_section_preset":"bg-gray-2","_uds_background_section_preset":"field_60381c933ba86"},"mode":"preview"} -->
+<!-- wp:columns -->
+<div class="wp-block-columns"><!-- wp:column -->
+<div class="wp-block-column"><!-- wp:heading {"style":{"spacing":{"margin":{"top":"var:preset|spacing|uds-size-4","bottom":"var:preset|spacing|uds-size-2"}}}} -->
+<h2 style="margin-top:var(--wp--preset--spacing--uds-size-4);margin-bottom:var(--wp--preset--spacing--uds-size-2)">One section, <span class="highlight-gold">two or three cards</span> across.</h2>
+<!-- /wp:heading --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
+
+<!-- wp:columns -->
+<div class="wp-block-columns"><!-- wp:column -->
+<div class="wp-block-column"><!-- wp:acf/uds-card {"name":"acf/uds-card","data":{"title":"Lorem Ipsum","_title":"field_602f0c315bfc0","body_text":"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.","_body_text":"field_602f0c412559a","card_style":"basic","_card_style":"field_602f0b39b4c29","header_style":"none","_header_style":"field_602f0b5660bc4","card_orientation":"vertical","_card_orientation":"field_602f0c2622823","buttons":"","_buttons":"field_602f0cc0abbc7","links":"","_links":"field_602f0e97702ae","tags":"","_tags":"field_602f0f084ce05"},"mode":"preview"} /--></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:acf/uds-card {"name":"acf/uds-card","data":{"title":"Lorem Ipsum","_title":"field_602f0c315bfc0","body_text":"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.","_body_text":"field_602f0c412559a","card_style":"basic","_card_style":"field_602f0b39b4c29","header_style":"none","_header_style":"field_602f0b5660bc4","card_orientation":"vertical","_card_orientation":"field_602f0c2622823","buttons":"","_buttons":"field_602f0cc0abbc7","links":"","_links":"field_602f0e97702ae","tags":"","_tags":"field_602f0f084ce05"},"mode":"preview"} /--></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:acf/uds-card {"name":"acf/uds-card","data":{"title":"Lorem Ipsum","_title":"field_602f0c315bfc0","body_text":"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.","_body_text":"field_602f0c412559a","card_style":"basic","_card_style":"field_602f0b39b4c29","header_style":"none","_header_style":"field_602f0b5660bc4","card_orientation":"vertical","_card_orientation":"field_602f0c2622823","buttons":"","_buttons":"field_602f0cc0abbc7","links":"","_links":"field_602f0e97702ae","tags":"","_tags":"field_602f0f084ce05"},"mode":"preview"} /--></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
+
+<!-- wp:columns -->
+<div class="wp-block-columns"><!-- wp:column -->
+<div class="wp-block-column"><!-- wp:buttons {"layout":{"type":"flex","justifyContent":"right"},"style":{"spacing":{"margin":{"bottom":"var:preset|spacing|uds-size-6"}}}} -->
+<div class="wp-block-buttons" style="margin-bottom:var(--wp--preset--spacing--uds-size-6)"><!-- wp:button {"backgroundColor":"asu-maroon","className":"is-style-"} -->
+<div class="wp-block-button is-style-"><a class="wp-block-button__link has-asu-maroon-background-color has-background wp-element-button">Call to action</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
+<!-- /wp:acf/background-section -->
