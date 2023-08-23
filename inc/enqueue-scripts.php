35832f70610516579b48008f96b88c428e3105f0
Steve Ryan <steve.ryan@asu.edu>
2023-08-23 15:33:23 -0700

    FIX: Enqueue scripts firing with incorrect hook

--- inc/enqueue-scripts.php
+++ inc/enqueue-scripts.php
@@ -16,7 +16,7 @@ function pitchfork_enqueue_scripts() {
 	$the_theme     = wp_get_theme();
 	$theme_version = $the_theme->get( 'Version' );
 
-	// Compiled SASS from ASU Boostrap + theme modifications.
+	// Global styles.
 	$css_version = $theme_version . '.' . filemtime( get_template_directory() . '/dist/css/theme.css' );
 	wp_enqueue_style( 'pitchfork-styles', get_template_directory_uri() . '/dist/css/theme.css', array(), $css_version );
 
@@ -35,21 +35,19 @@ function pitchfork_enqueue_scripts() {
 	wp_enqueue_script ( 'component-cookie', get_template_directory_uri()  . '/src/component-cookie-consent/js/asuCookieConsent.umd.js', array( 'wp-element', 'wp-components' ), null, false );
 
 	// Custom scripts from the theme. Includes init files for above components.
-	$custom_js_version = $theme_version . '.' . filemtime( get_template_directory() . '/dist/js/custom.min.js' );
-	wp_enqueue_script( 'pitchfork-custom', get_template_directory_uri() . '/dist/js/custom.min.js', array( 'bootstrap-bundle' ), $custom_js_version, true );
+	$custom_js_version = $theme_version . '.' . filemtime( get_template_directory() . '/dist/js/custom.js' );
+	wp_enqueue_script( 'pitchfork-custom', get_template_directory_uri() . '/dist/js/custom.js', array('bootstrap-bundle'), $custom_js_version, true );
 
 	// Font Awesome. Kit licensed and distributed by ASU Engineering for use with this theme.
 	wp_enqueue_script ( 'font-awesome-kit', 'https://kit.fontawesome.com/51b562cd96.js', array(), null, false );
 	wp_script_add_data( 'font-awesome-kit', 'crossorigin', 'anonymous' );
-
 }
 add_action( 'wp_enqueue_scripts', 'pitchfork_enqueue_scripts' );
-
 
 /**
  * Register/enqueue scripts for Pitchfork.
  */
-function pitchfork_enqueue_admin_scripts() {
+function pitchfork_enqueue_block_editor_assets() {
 
 	// Get the theme data.
 	$the_theme     = wp_get_theme();
@@ -71,20 +69,20 @@ function pitchfork_enqueue_admin_scripts() {
 	$js_heading_highlights = $theme_version . '.' . filemtime( get_template_directory() . '/dist/js/heading-highlights.js' );
 	wp_enqueue_script( 'uds-heading-highlights', get_template_directory_uri() . '/dist/js/heading-highlights.js', array( 'wp-rich-text', 'wp-element', 'wp-editor' ), $js_heading_highlights );
 
-}
-add_action( 'admin_enqueue_scripts', 'pitchfork_enqueue_admin_scripts' );
-
-/**
- * Register/enqueue scripts for the block editor.
- */
-function pitchfork_enqueue_block_scripts() {
-
-	// Get the theme data.
-	$the_theme     = wp_get_theme();
-	$theme_version = $the_theme->get( 'Version' );
-
 	// Block modifications.
 	$blocks_js_version = $theme_version . '.' . filemtime( get_template_directory() . '/dist/js/block-variations.js' );
 	wp_enqueue_script( 'pitchfork-block-mods', get_template_directory_uri() . '/dist/js/block-variations.js',  array( 'wp-blocks', 'wp-dom' ), $blocks_js_version , true );
 }
-add_action( 'enqueue_block_editor_assets', 'pitchfork_enqueue_block_scripts' );
+add_action( 'enqueue_block_assets', 'pitchfork_enqueue_block_editor_assets' );
+
+/**
+ * Register/enqueue scripts for the block editor.
+ */
+// function pitchfork_enqueue_block_scripts() {
+
+// 	// Get the theme data.
+// 	$the_theme     = wp_get_theme();
+// 	$theme_version = $the_theme->get( 'Version' );
+
+// }
+// add_action( 'enqueue_block_editor_assets', 'pitchfork_enqueue_block_scripts' );
