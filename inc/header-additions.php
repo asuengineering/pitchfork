77b26defddd9d2a6d643efc742c0c2178bc7696d
Steve Ryan <steve.ryan@asu.edu>
2024-02-12 11:36:17 -0700

    Correcting image path to favicons.
    Relocated due to new build process with the theme. Image path needed to be reset to correct location.

--- inc/header-additions.php
+++ inc/header-additions.php
@@ -12,27 +12,27 @@ defined( 'ABSPATH' ) || exit;
  * Theme favicons header hook, provides links to the favicons from the UDS WordPress Theme
  */
 function pitchfork_add_favicons_to_header() {
-	?>
-<link rel="apple-touch-icon-precomposed" sizes="57x57" href="<?php echo get_template_directory_uri(); ?>/src/img/asu-unity/favicon/apple-touch-icon-57x57.png" />
-<link rel="apple-touch-icon-precomposed" sizes="114x114" href="<?php echo get_template_directory_uri(); ?>/src/img/asu-unity/favicon/apple-touch-icon-114x114.png" />
-<link rel="apple-touch-icon-precomposed" sizes="72x72" href="<?php echo get_template_directory_uri(); ?>/src/img/asu-unity/favicon/apple-touch-icon-72x72.png" />
-<link rel="apple-touch-icon-precomposed" sizes="144x144" href="<?php echo get_template_directory_uri(); ?>/src/img/asu-unity/favicon/apple-touch-icon-144x144.png" />
-<link rel="apple-touch-icon-precomposed" sizes="60x60" href="<?php echo get_template_directory_uri(); ?>/src/img/asu-unity/favicon/apple-touch-icon-60x60.png" />
-<link rel="apple-touch-icon-precomposed" sizes="120x120" href="<?php echo get_template_directory_uri(); ?>/src/img/asu-unity/favicon/apple-touch-icon-120x120.png" />
-<link rel="apple-touch-icon-precomposed" sizes="76x76" href="<?php echo get_template_directory_uri(); ?>/src/img/asu-unity/favicon/apple-touch-icon-76x76.png" />
-<link rel="apple-touch-icon-precomposed" sizes="152x152" href="<?php echo get_template_directory_uri(); ?>/src/img/asu-unity/favicon/apple-touch-icon-152x152.png" />
-<link rel="icon" type="image/png" href="<?php echo get_template_directory_uri(); ?>/src/img/asu-unity/favicon/favicon-196x196.png" sizes="196x196" />
-<link rel="icon" type="image/png" href="<?php echo get_template_directory_uri(); ?>/src/img/asu-unity/favicon/favicon-96x96.png" sizes="96x96" />
-<link rel="icon" type="image/png" href="<?php echo get_template_directory_uri(); ?>/src/img/asu-unity/favicon/favicon-32x32.png" sizes="32x32" />
-<link rel="icon" type="image/png" href="<?php echo get_template_directory_uri(); ?>/src/img/asu-unity/favicon/favicon-16x16.png" sizes="16x16" />
-<link rel="icon" type="image/png" href="<?php echo get_template_directory_uri(); ?>/src/img/asu-unity/favicon/favicon-128.png" sizes="128x128" />
+	?>/Users/tfserwin/LocalWP/pitchfork/app/public/wp-content/themes/pitchfork/src/unity-bootstrap-theme/img/favicon
+<link rel="apple-touch-icon-precomposed" sizes="57x57" href="<?php echo get_template_directory_uri(); ?>/src/unity-bootstrap-theme/img/favicon/apple-touch-icon-57x57.png" />
+<link rel="apple-touch-icon-precomposed" sizes="114x114" href="<?php echo get_template_directory_uri(); ?>/src/unity-bootstrap-theme/img/favicon/apple-touch-icon-114x114.png" />
+<link rel="apple-touch-icon-precomposed" sizes="72x72" href="<?php echo get_template_directory_uri(); ?>/src/unity-bootstrap-theme/img/favicon/apple-touch-icon-72x72.png" />
+<link rel="apple-touch-icon-precomposed" sizes="144x144" href="<?php echo get_template_directory_uri(); ?>/src/unity-bootstrap-theme/img/favicon/apple-touch-icon-144x144.png" />
+<link rel="apple-touch-icon-precomposed" sizes="60x60" href="<?php echo get_template_directory_uri(); ?>/src/unity-bootstrap-theme/img/favicon/apple-touch-icon-60x60.png" />
+<link rel="apple-touch-icon-precomposed" sizes="120x120" href="<?php echo get_template_directory_uri(); ?>/src/unity-bootstrap-theme/img/favicon/apple-touch-icon-120x120.png" />
+<link rel="apple-touch-icon-precomposed" sizes="76x76" href="<?php echo get_template_directory_uri(); ?>/src/unity-bootstrap-theme/img/favicon/apple-touch-icon-76x76.png" />
+<link rel="apple-touch-icon-precomposed" sizes="152x152" href="<?php echo get_template_directory_uri(); ?>/src/unity-bootstrap-theme/img/favicon/apple-touch-icon-152x152.png" />
+<link rel="icon" type="image/png" href="<?php echo get_template_directory_uri(); ?>/src/unity-bootstrap-theme/img/favicon/favicon-196x196.png" sizes="196x196" />
+<link rel="icon" type="image/png" href="<?php echo get_template_directory_uri(); ?>/src/unity-bootstrap-theme/img/favicon/favicon-96x96.png" sizes="96x96" />
+<link rel="icon" type="image/png" href="<?php echo get_template_directory_uri(); ?>/src/unity-bootstrap-theme/img/favicon/favicon-32x32.png" sizes="32x32" />
+<link rel="icon" type="image/png" href="<?php echo get_template_directory_uri(); ?>/src/unity-bootstrap-theme/img/favicon/favicon-16x16.png" sizes="16x16" />
+<link rel="icon" type="image/png" href="<?php echo get_template_directory_uri(); ?>/src/unity-bootstrap-theme/img/favicon/favicon-128.png" sizes="128x128" />
 <meta name="application-name" content="<?php echo bloginfo( 'name' ); ?>"/>
 <meta name="msapplication-TileColor" content="#FFFFFF" />
-<meta name="msapplication-TileImage" content="<?php echo get_template_directory_uri(); ?>/src/img/asu-unity/favicon/mstile-144x144.png" />
-<meta name="msapplication-square70x70logo" content="<?php echo get_template_directory_uri(); ?>/src/img/asu-unity/favicon/mstile-70x70.png" />
-<meta name="msapplication-square150x150logo" content="<?php echo get_template_directory_uri(); ?>/src/img/asu-unity/favicon/mstile-150x150.png" />
-<meta name="msapplication-wide310x150logo" content="<?php echo get_template_directory_uri(); ?>/src/img/asu-unity/favicon/mstile-310x150.png" />
-<meta name="msapplication-square310x310logo" content="<?php echo get_template_directory_uri(); ?>/src/img/asu-unity/favicon/mstile-310x310.png" />
+<meta name="msapplication-TileImage" content="<?php echo get_template_directory_uri(); ?>/src/unity-bootstrap-theme/img/favicon/mstile-144x144.png" />
+<meta name="msapplication-square70x70logo" content="<?php echo get_template_directory_uri(); ?>/src/unity-bootstrap-theme/img/favicon/mstile-70x70.png" />
+<meta name="msapplication-square150x150logo" content="<?php echo get_template_directory_uri(); ?>/src/unity-bootstrap-theme/img/favicon/mstile-150x150.png" />
+<meta name="msapplication-wide310x150logo" content="<?php echo get_template_directory_uri(); ?>/src/unity-bootstrap-theme/img/favicon/mstile-310x150.png" />
+<meta name="msapplication-square310x310logo" content="<?php echo get_template_directory_uri(); ?>/src/unity-bootstrap-theme/img/favicon/mstile-310x310.png" />
 
 	<?php
 }
