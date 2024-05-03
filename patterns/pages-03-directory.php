b52df1833792b0041f380eab3db9fa29174ccfe9
Steve Ryan <steve.ryan@asu.edu>
2024-05-03 16:29:02 -0700

    Reorganize existing patterns

---
+++ patterns/pages-03-directory.php
@@ -0,0 +1,94 @@
+<?php
+/**
+ * Title: Page: Directory
+ * Slug: pitchfork/pages-03-directory
+ * Categories: pitchfork-fullpage
+ * Block Types: core
+ */
+?>
+
+<!-- wp:acf/hero {"name":"acf/hero","data":{"uds_hero_size":"uds-hero-md","_uds_hero_size":"field_611425a016b0e","uds_hero_image":"","_uds_hero_image":"field_6114269916b0f","uds_hero_content_on_mobile":"1","_uds_hero_content_on_mobile":"field_611aa5d4d076a"},"mode":"preview"} -->
+<!-- wp:acf/subtitle {"name":"acf/subtitle","data":{"uds_subtitle_text":"Department name","_uds_subtitle_text":"field_6250abe44b020","uds_subtitle_highlight_color":"highlight-gold","_uds_subtitle_highlight_color":"field_6250ac5e4b021"},"mode":"preview"} /-->
+
+<!-- wp:heading {"level":1} -->
+<h1 class="wp-block-heading">Building a directory page</h1>
+<!-- /wp:heading -->
+
+<!-- wp:group {"className":"content"} -->
+<div class="wp-block-group content"><!-- wp:paragraph -->
+<p>A whole-page pattern composed of repeatable content sections.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+<!-- /wp:acf/hero -->
+
+<!-- wp:heading -->
+<h2 class="wp-block-heading" id="principal-investigator">Here is <span class="highlight-black">our leader</span> and principal investigator.</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"className":"is-style-lead"} -->
+<p class="is-style-lead">Add your own description here for context or further classification.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:acf/profiles {"name":"acf/profiles","data":{"uds_profiles_columns":"col-one","_uds_profiles_columns":"field_63c98c07e138e"},"mode":"preview"} -->
+<!-- wp:acf/profile-data {"name":"acf/profile-data","data":{"field_64557ce1fd8cc":"squires"},"mode":"preview","className":"is-style-large"} /-->
+<!-- /wp:acf/profiles -->
+
+<!-- wp:spacer {"height":"8rem"} -->
+<div style="height:8rem" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:heading -->
+<h2 class="wp-block-heading">For when you need&nbsp;<span class="highlight-gold">important people</span>&nbsp;side-by-side.</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"className":"is-style-lead"} -->
+<p class="is-style-lead">The profile-manual blocks have no default profile image. Be sure to upload your own image in the block settings.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:acf/profiles {"name":"acf/profiles","data":{"uds_profiles_columns":"col-two","_uds_profiles_columns":"field_63c98c07e138e"},"mode":"preview","style":{"spacing":{"margin":{"top":"var:preset|spacing|uds-size-6","bottom":"var:preset|spacing|uds-size-6"}}}} -->
+<!-- wp:acf/profile-data {"name":"acf/profile-data","data":{"field_64557ce1fd8cc":"collofel"},"mode":"preview","className":"is-style-large","backgroundColor":"fill-white"} /-->
+
+<!-- wp:acf/profile-manual {"name":"acf/profile-manual","data":{"uds_profilemanual_name":"Default Name","_uds_profilemanual_name":"field_63bf28de08674","uds_profilemanual_image":"","_uds_profilemanual_image":"field_63bf2b7c0aec6","uds_profilemanual_url":"https://search.asu.edu","_uds_profilemanual_url":"field_63bf2e0a0aec8","uds_profilemanual_title":"Trusted Faculty","_uds_profilemanual_title":"field_63bf295b0aec1","uds_profilemanual_department":"ASU Engineering","_uds_profilemanual_department":"field_63bf298c0aec2","uds_profilemanual_description":"ASU is a comprehensive public research university, measured not by whom it excludes, but by whom it includes and how they succeed; advancing research and discovery of public value; and assuming fundamental responsibility for the economic, social, cultural and overall health of the communities it serves.","_uds_profilemanual_description":"field_63bf2c770aec7","uds_profilemanual_email":"email@asu.edu","_uds_profilemanual_email":"field_63bf299a0aec3","uds_profilemanual_phone":"480-123-4567","_uds_profilemanual_phone":"field_63bf2a420aec4","uds_profilemanual_address":"","_uds_profilemanual_address":"field_63bf2b400aec5","uds_profilemanual_social":"","_uds_profilemanual_social":"field_63c82588b90ee"},"mode":"preview","className":"is-style-large","backgroundColor":"fill-white"} /-->
+<!-- /wp:acf/profiles -->
+
+<!-- wp:spacer {"height":"8rem"} -->
+<div style="height:8rem" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:heading -->
+<h2 class="wp-block-heading"><span class="highlight-gold">Smaller</span> individual profiles. Still in <span class="highlight-gold">two columns.</span></h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"className":"is-style-lead"} -->
+<p class="is-style-lead">Each block comes in three different styles:&nbsp;<strong>no-outline</strong>,&nbsp;<strong>fill-white</strong>,&nbsp;<strong>fill-grey</strong>.<br>Each block also has three sizes:&nbsp;<strong>large</strong>,&nbsp;<strong>small</strong>&nbsp;and&nbsp;<strong>micro</strong>.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:acf/profiles {"name":"acf/profiles","data":{"uds_profiles_columns":"col-two","_uds_profiles_columns":"field_63c98c07e138e"},"mode":"preview","style":{"spacing":{"margin":{"top":"var:preset|spacing|uds-size-6","bottom":"var:preset|spacing|uds-size-6"}}}} -->
+<!-- wp:acf/profile-data {"name":"acf/profile-data","data":{"field_64557ce1fd8cc":"nrolston"},"mode":"preview","backgroundColor":"fill"} /-->
+
+<!-- wp:acf/profile-manual {"name":"acf/profile-manual","data":{"uds_profilemanual_name":"Default Name","_uds_profilemanual_name":"field_63bf28de08674","uds_profilemanual_image":"","_uds_profilemanual_image":"field_63bf2b7c0aec6","uds_profilemanual_url":"https://search.asu.edu","_uds_profilemanual_url":"field_63bf2e0a0aec8","uds_profilemanual_title":"Trusted Faculty","_uds_profilemanual_title":"field_63bf295b0aec1","uds_profilemanual_department":"ASU Engineering","_uds_profilemanual_department":"field_63bf298c0aec2","uds_profilemanual_description":"ASU is a comprehensive public research university, measured not by whom it excludes, but by whom it includes and how they succeed; advancing research and discovery of public value; and assuming fundamental responsibility for the economic, social, cultural and overall health of the communities it serves.","_uds_profilemanual_description":"field_63bf2c770aec7","uds_profilemanual_email":"email@asu.edu","_uds_profilemanual_email":"field_63bf299a0aec3","uds_profilemanual_phone":"480-123-4567","_uds_profilemanual_phone":"field_63bf2a420aec4","uds_profilemanual_address":"","_uds_profilemanual_address":"field_63bf2b400aec5","uds_profilemanual_social":"","_uds_profilemanual_social":"field_63c82588b90ee"},"mode":"preview","className":"is-style-small","backgroundColor":"fill-white"} /-->
+<!-- /wp:acf/profiles -->
+
+<!-- wp:spacer {"height":"8rem"} -->
+<div style="height:8rem" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:heading -->
+<h2 class="wp-block-heading">Directory section - three columns, <span class="highlight-gold">micro</span></h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"className":"is-style-lead"} -->
+<p class="is-style-lead">Documentation for the background treatments and other settings can be found within the <a href="https://comm.engineering.asu.edu/pitchfork/doc-category/pitchfork-people/">plugin documentation</a>.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:acf/profiles {"name":"acf/profiles","data":{"field_63c98c07e138e":"col-three"},"mode":"preview","style":{"spacing":{"margin":{"top":"var:preset|spacing|uds-size-6","bottom":"var:preset|spacing|uds-size-6"}}}} -->
+<!-- wp:acf/profile-data {"name":"acf/profile-data","data":{"field_64557ce1fd8cc":"zcholman"},"mode":"preview","className":"is-style-micro"} /-->
+
+<!-- wp:acf/profile-manual {"name":"acf/profile-manual","data":{"uds_profilemanual_name":"Default Name","_uds_profilemanual_name":"field_63bf28de08674","uds_profilemanual_image":"","_uds_profilemanual_image":"field_63bf2b7c0aec6","uds_profilemanual_url":"https://search.asu.edu","_uds_profilemanual_url":"field_63bf2e0a0aec8","uds_profilemanual_title":"Trusted Faculty","_uds_profilemanual_title":"field_63bf295b0aec1","uds_profilemanual_department":"ASU Engineering","_uds_profilemanual_department":"field_63bf298c0aec2","uds_profilemanual_description":"ASU is a comprehensive public research university, measured not by whom it excludes, but by whom it includes and how they succeed; advancing research and discovery of public value; and assuming fundamental responsibility for the economic, social, cultural and overall health of the communities it serves.","_uds_profilemanual_description":"field_63bf2c770aec7","uds_profilemanual_email":"email@asu.edu","_uds_profilemanual_email":"field_63bf299a0aec3","uds_profilemanual_phone":"480-123-4567","_uds_profilemanual_phone":"field_63bf2a420aec4","uds_profilemanual_address":"","_uds_profilemanual_address":"field_63bf2b400aec5","uds_profilemanual_social":"","_uds_profilemanual_social":"field_63c82588b90ee"},"mode":"preview","className":"is-style-micro has-default-img"} /-->
+
+<!-- wp:acf/profile-manual {"name":"acf/profile-manual","data":{"uds_profilemanual_name":"Default Name","_uds_profilemanual_name":"field_63bf28de08674","uds_profilemanual_image":"","_uds_profilemanual_image":"field_63bf2b7c0aec6","uds_profilemanual_url":"https://search.asu.edu","_uds_profilemanual_url":"field_63bf2e0a0aec8","uds_profilemanual_title":"Trusted Faculty","_uds_profilemanual_title":"field_63bf295b0aec1","uds_profilemanual_department":"ASU Engineering","_uds_profilemanual_department":"field_63bf298c0aec2","uds_profilemanual_description":"ASU is a comprehensive public research university, measured not by whom it excludes, but by whom it includes and how they succeed; advancing research and discovery of public value; and assuming fundamental responsibility for the economic, social, cultural and overall health of the communities it serves.","_uds_profilemanual_description":"field_63bf2c770aec7","uds_profilemanual_email":"email@asu.edu","_uds_profilemanual_email":"field_63bf299a0aec3","uds_profilemanual_phone":"480-123-4567","_uds_profilemanual_phone":"field_63bf2a420aec4","uds_profilemanual_address":"","_uds_profilemanual_address":"field_63bf2b400aec5","uds_profilemanual_social":"","_uds_profilemanual_social":"field_63c82588b90ee"},"mode":"preview","className":"is-style-micro"} /-->
+
+<!-- wp:acf/profile-manual {"name":"acf/profile-manual","data":{"uds_profilemanual_name":"Default Name","_uds_profilemanual_name":"field_63bf28de08674","uds_profilemanual_image":"","_uds_profilemanual_image":"field_63bf2b7c0aec6","uds_profilemanual_url":"https://search.asu.edu","_uds_profilemanual_url":"field_63bf2e0a0aec8","uds_profilemanual_title":"Trusted Faculty","_uds_profilemanual_title":"field_63bf295b0aec1","uds_profilemanual_department":"ASU Engineering","_uds_profilemanual_department":"field_63bf298c0aec2","uds_profilemanual_description":"ASU is a comprehensive public research university, measured not by whom it excludes, but by whom it includes and how they succeed; advancing research and discovery of public value; and assuming fundamental responsibility for the economic, social, cultural and overall health of the communities it serves.","_uds_profilemanual_description":"field_63bf2c770aec7","uds_profilemanual_email":"email@asu.edu","_uds_profilemanual_email":"field_63bf299a0aec3","uds_profilemanual_phone":"480-123-4567","_uds_profilemanual_phone":"field_63bf2a420aec4","uds_profilemanual_address":"","_uds_profilemanual_address":"field_63bf2b400aec5","uds_profilemanual_social":"","_uds_profilemanual_social":"field_63c82588b90ee"},"mode":"preview","className":"is-style-micro","backgroundColor":"fill"} /-->
+
+<!-- wp:acf/profile-manual {"name":"acf/profile-manual","data":{"uds_profilemanual_name":"Default Name","_uds_profilemanual_name":"field_63bf28de08674","uds_profilemanual_image":"","_uds_profilemanual_image":"field_63bf2b7c0aec6","uds_profilemanual_url":"https://search.asu.edu","_uds_profilemanual_url":"field_63bf2e0a0aec8","uds_profilemanual_title":"Trusted Faculty","_uds_profilemanual_title":"field_63bf295b0aec1","uds_profilemanual_department":"ASU Engineering","_uds_profilemanual_department":"field_63bf298c0aec2","uds_profilemanual_description":"ASU is a comprehensive public research university, measured not by whom it excludes, but by whom it includes and how they succeed; advancing research and discovery of public value; and assuming fundamental responsibility for the economic, social, cultural and overall health of the communities it serves.","_uds_profilemanual_description":"field_63bf2c770aec7","uds_profilemanual_email":"email@asu.edu","_uds_profilemanual_email":"field_63bf299a0aec3","uds_profilemanual_phone":"480-123-4567","_uds_profilemanual_phone":"field_63bf2a420aec4","uds_profilemanual_address":"","_uds_profilemanual_address":"field_63bf2b400aec5","uds_profilemanual_social":"","_uds_profilemanual_social":"field_63c82588b90ee"},"mode":"preview","className":"is-style-micro","backgroundColor":"fill-white"} /-->
+<!-- /wp:acf/profiles -->
---
+++ patterns/section-01-directory-main.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Title: Directory - Principal investigator
+ * Slug: pitchfork/section-01-directory-main
+ * Categories: pitchfork-section
+ * Block Types: core
+ */
+?>
+
+<!-- wp:heading -->
+<h2 class="wp-block-heading" id="principal-investigator">Here is <span class="highlight-black">our leader</span> and principal investigator.</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"className":"is-style-lead"} -->
+<p class="is-style-lead">Add your own description here for context or further classification.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:acf/profiles {"name":"acf/profiles","data":{"uds_profiles_columns":"col-one","_uds_profiles_columns":"field_63c98c07e138e"},"mode":"preview"} -->
+<!-- wp:acf/profile-data {"name":"acf/profile-data","data":{"field_64557ce1fd8cc":"squires"},"mode":"preview","className":"is-style-large"} /-->
+<!-- /wp:acf/profiles -->
