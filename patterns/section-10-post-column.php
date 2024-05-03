2228ddf5c03bf04e77cca5d1bce72c4108700d0c
Steve Ryan <steve.ryan@asu.edu>
2024-05-03 16:28:50 -0700

    Add post column pattern to library

---
+++ patterns/section-10-post-column.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Title: Post Column Layout
+ * Slug: pitchfork/section-10-post-column
+ * Categories: pitchfork-section
+ * Block Types: core
+ */
+?>
+
+
+
+<!-- wp:query {"queryId":0,"query":{"perPage":3,"pages":0,"offset":"0","postType":"post","order":"desc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"","inherit":false,"taxQuery":{"category":[]}}} -->
+<div class="wp-block-query"><!-- wp:post-template {"metadata":{"name":"Post Column"},"className":"is-style-post-column"} -->
+<!-- wp:post-featured-image /-->
+
+<!-- wp:post-title {"level":3,"style":{"spacing":{"margin":{"bottom":"0"}}}} /-->
+
+<!-- wp:post-date /-->
+
+<!-- wp:post-excerpt {"style":{"spacing":{"margin":{"top":"var:preset|spacing|uds-size-2"}}}} /-->
+
+<!-- wp:separator -->
+<hr class="wp-block-separator has-alpha-channel-opacity"/>
+<!-- /wp:separator -->
+<!-- /wp:post-template --></div>
+<!-- /wp:query -->
