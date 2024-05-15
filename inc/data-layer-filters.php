408346b346e2708c1a742d654a6aead858ea6725
Steve Ryan <steve.ryan@asu.edu>
2024-05-15 16:40:50 -0700

    Add data-layer tracking to button+label in editor

--- inc/data-layer-filters.php
+++ inc/data-layer-filters.php
@@ -14,29 +14,35 @@
 
 
 /**
- * Adds data layer elements for every button block on the page.
- *
- * 20-Sept-2023: We're not quite ready for this one yet.
- * We will likely want to do this on a conditional basis. Keeping around as a working example.
- * Uncomment the render_block filter to enable.
+ * Add data layer tracking for buttons that also have distinct labels in the editor.
  */
 function pitchfork_datalayer_core_button( $block_content, $block ) {
+
+	// Looking for core buttons and excluding blocks with no visible just yet.
     if ( ! $block_content || $block['blockName'] !== 'core/button' ) {
         return $block_content;
     }
 
-    $processor = new WP_HTML_Tag_Processor( $block_content );
+	// Check for existence of a block name in the metadata. Return early if none found.
+	if (! empty($block['attrs']['metadata']['name'])) {
+		$blockname = $block['attrs']['metadata']['name'];
+	} else {
+		return $block_content;
+	}
 
-    if ( $processor->next_tag( array( 'class_name' => 'wp-element-button' ) ) ) {
-		do_action('qm/debug', $block_content);
-        $processor->set_attribute( 'data-ga', wp_strip_all_tags($block_content));
+	// If we are still here, add the block name to the datalayer elements and render block.
+	$processor = new WP_HTML_Tag_Processor( $block_content );
+
+	if ( $processor->next_tag( array( 'class_name' => 'wp-element-button' ) ) ) {
+		$processor->set_attribute( 'data-ga', $blockname);
 		$processor->set_attribute( 'data-ga-name', 'onclick' );
 		$processor->set_attribute( 'data-ga-event', 'link' );
 		$processor->set_attribute( 'data-ga-action', 'click' );
 		$processor->set_attribute( 'data-ga-type', 'internal link' );
 		$processor->set_attribute( 'data-ga-region', 'main content' );
-    }
+	}
 
-    return $processor->get_updated_html();
+	return $processor->get_updated_html();
+
 }
-// add_filter( 'render_block', 'pitchfork_datalayer_core_button', 10, 2 );
+add_filter( 'render_block', 'pitchfork_datalayer_core_button', 10, 2 );
