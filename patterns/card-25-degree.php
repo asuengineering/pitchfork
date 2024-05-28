b0d14c2da79a834ea0793c003f91fd4baa1a2b37
Steve Ryan <steve.ryan@asu.edu>
2024-05-28 14:06:53 -0700

    Add back missing card:degree pattern

---
+++ patterns/card-25-degree.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Title: Card: Degree
+ * Slug: pitchfork/card-degree
+ * Categories: pitchfork-card
+ * Block Types: core
+ */
+?>
+
+<!-- wp:acf/card-v2 {"name":"acf/card-v2","data":{"uds_card2_orientation":"vertical","_uds_card2_orientation":"field_6605e470e79dc"},"mode":"preview","className":"is-style-card-degree"} -->
+<!-- wp:acf/card-v2-image {"name":"acf/card-v2-image","data":{"field_6622a98c9a652":""},"mode":"preview","lock":{"move":true,"remove":false}} /-->
+
+<!-- wp:acf/card-v2-header {"name":"acf/card-v2-header","mode":"preview","lock":{"move":true,"remove":true}} -->
+<!-- wp:heading {"level":3,"placeholder":"Degree card title, MS"} -->
+<h3 class="wp-block-heading"></h3>
+<!-- /wp:heading -->
+<!-- /wp:acf/card-v2-header -->
+
+<!-- wp:group {"lock":{"move":true,"remove":true},"metadata":{"name":"Card Content"}} -->
+<div class="wp-block-group"><!-- wp:paragraph {"placeholder":"Card body copy goes here."} -->
+<p></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:buttons {"lock":{"move":true,"remove":false},"layout":{"type":"flex","justifyContent":"left","orientation":"vertical","flexWrap":"wrap"}} -->
+<div class="wp-block-buttons"><!-- wp:button {"backgroundColor":"asu-maroon","lock":{"move":true,"remove":false},"className":"is-style-"} -->
+<div class="wp-block-button is-style-"><a class="wp-block-button__link has-asu-maroon-background-color has-background wp-element-button">Degree Details</a></div>
+<!-- /wp:button -->
+
+<!-- wp:button {"backgroundColor":"asu-gold","lock":{"move":true,"remove":false},"className":"is-style-"} -->
+<div class="wp-block-button is-style-"><a class="wp-block-button__link has-asu-gold-background-color has-background wp-element-button">Apply</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons -->
+<!-- /wp:acf/card-v2 -->
