e7aeccce0c720e42387374e778110bf0fc628ce2
Steve Ryan <steve.ryan@asu.edu>
2025-10-31 13:22:38 -0700

    Add inset content box pattern

---
+++ patterns/content-20-inset-content-box.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Title: Content: Inset content box
+ * Slug: pitchfork/content-inset-box
+ * Categories: pitchfork-content
+ * Block Types: core
+ */
+?>
+
+<!-- wp:acf/background-section {"name":"acf/background-section","data":{"field_603819ea114a9":"none"},"mode":"preview","backgroundColor":"gray-2"} -->
+<!-- wp:group {"metadata":{"name":"Content width"},"layout":{"type":"constrained","contentSize":"60rem"}} -->
+<div class="wp-block-group"><!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|uds-size-4","bottom":"var:preset|spacing|uds-size-4","left":"var:preset|spacing|uds-size-4","right":"var:preset|spacing|uds-size-4"}}},"backgroundColor":"white","layout":{"type":"constrained"}} -->
+<div class="wp-block-group has-white-background-color has-background" style="padding-top:var(--wp--preset--spacing--uds-size-4);padding-right:var(--wp--preset--spacing--uds-size-4);padding-bottom:var(--wp--preset--spacing--uds-size-4);padding-left:var(--wp--preset--spacing--uds-size-4)">
+
+<!-- wp:heading {"level":3,"placeholder":"Add a section heading…"} -->
+<h3 class="wp-block-heading"></h3>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"placeholder":"Add an introductory paragraph or summary…"} -->
+<p></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"placeholder":"Add additional supporting details…"} -->
+<p></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:buttons -->
+<div class="wp-block-buttons"><!-- wp:button -->
+<div class="wp-block-button"><a class="wp-block-button__link wp-element-button">Default button</a></div>
+<!-- /wp:button -->
+
+<!-- wp:button -->
+<div class="wp-block-button"><a class="wp-block-button__link wp-element-button">Secondary CTA</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons -->
+
+</div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
+<!-- /wp:acf/background-section -->
+
