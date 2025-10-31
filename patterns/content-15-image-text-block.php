ea7013c33d44f63f22a5d6ddd20f6f76711f1ac1
Steve Ryan <steve.ryan@asu.edu>
2025-10-31 10:24:05 -0700

    Add image and text block pattern using core blocks

---
+++ patterns/content-15-image-text-block.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Title: Content: Image and text block
+ * Slug: pitchfork/content-image-text
+ * Categories: pitchfork-content
+ * Block Types: core
+ */
+?>
+
+<!-- wp:media-text {"mediaId":0,"mediaLink":"https://picsum.photos/id/155/800/800","mediaType":"image","verticalAlignment":"top","imageFill":true,"focalPoint":{"x":0.5,"y":0.5},"metadata":{"categories":["pitchfork-content"],"patternName":"pitchfork/content-image-text","name":"Image and text block"},"style":{"border":{"width":"2px"},"spacing":{"padding":{"top":"0","bottom":"0","left":"0","right":"0"}}},"backgroundColor":"white","borderColor":"gray-2"} -->
+<div class="wp-block-media-text is-stacked-on-mobile is-vertically-aligned-top is-image-fill-element has-border-color has-gray-2-border-color has-white-background-color has-background" style="border-width:2px;padding-top:0;padding-right:0;padding-bottom:0;padding-left:0"><figure class="wp-block-media-text__media"><img src="https://picsum.photos/id/155/800/800" alt="Add alt text to your image" style="object-position:50% 50%"/></figure><div class="wp-block-media-text__content"><!-- wp:heading {"placeholder":"Add a headline…","style":{"spacing":{"margin":{"top":"var:preset|spacing|uds-size-4"}}}} -->
+<h2 class="wp-block-heading" style="margin-top:var(--wp--preset--spacing--uds-size-4)"></h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"placeholder":"Add a short intro or summary…"} -->
+<p></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"placeholder":"Add supporting paragraph text…"} -->
+<p></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:buttons {"style":{"spacing":{"padding":{"top":"var:preset|spacing|uds-size-4","bottom":"var:preset|spacing|uds-size-4"}}}} -->
+<div class="wp-block-buttons" style="padding-top:var(--wp--preset--spacing--uds-size-4);padding-bottom:var(--wp--preset--spacing--uds-size-4)"><!-- wp:button {"placeholder":"Add button label…"} -->
+<div class="wp-block-button"><a class="wp-block-button__link wp-element-button"></a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div></div>
+<!-- /wp:media-text -->
