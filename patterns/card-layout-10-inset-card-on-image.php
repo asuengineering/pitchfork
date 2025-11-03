16fe7ab07ebd5ebc65e77b456db9f1666098032a
Steve Ryan <steve.ryan@asu.edu>
2025-11-03 15:49:13 -0700

    Add missing class to pattern

--- patterns/card-layout-10-inset-card-on-image.php
+++ patterns/card-layout-10-inset-card-on-image.php
@@ -9,7 +9,7 @@
 
 <!-- wp:acf/background-section {"name":"acf/background-section","data":{"field_603819ea114a9":"upload","field_60381d8c3ba87":"","field_606e31f365933":"no-repeat","field_606e340a65934":"center","field_607f2bc905fd6":"cover","field_606e315965932":""},"mode":"preview","className":"pitchfork-inset-card-pattern","backgroundColor":"gray-2","metadata":{"categories":["pitchfork-card-layouts"],"patternName":"pitchfork/cards-inset-on-image","name":"Card layout: Inset card on image"}} -->
 <!-- wp:group {"lock":{"move":true,"remove":true},"metadata":{"name":"Content width (62.5rem)"},"layout":{"type":"constrained","contentSize":"62.25rem","justifyContent":"center"}} -->
-<div class="wp-block-group"><!-- wp:group {"lock":{"move":true,"remove":true},"metadata":{"name":"Card width (24rem 3col)"},"className":"pitchfork-inset-card-width","layout":{"type":"constrained","contentSize":"24rem","justifyContent":"right"}} -->
+<div class="wp-block-group pitchfork-inset-card-width"><!-- wp:group {"lock":{"move":true,"remove":true},"metadata":{"name":"Card width (24rem 3col)"},"className":"pitchfork-inset-card-width","layout":{"type":"constrained","contentSize":"24rem","justifyContent":"right"}} -->
 <div class="wp-block-group"><!-- wp:acf/card-v2 {"name":"acf/card-v2","data":{"uds_card2_orientation":"vertical","_uds_card2_orientation":"field_6605e470e79dc"},"mode":"preview","metadata":{"categories":["pitchfork-card"],"patternName":"pitchfork/card-icon","name":"Card: Icon Header"}} -->
 <!-- wp:acf/card-v2-icon {"name":"acf/card-v2-icon","data":{"uds_card2_icon":"","_uds_card2_icon":"field_6605e783c68b3"},"mode":"preview","lock":{"move":true,"remove":false}} /-->
 
