e9bbad1f971bc7ebed01bbe98c643345c0d4c64a
Steve Ryan <steve.ryan@asu.edu>
2026-04-01 10:37:54 -0700

    Remove console.log statements

--- src/scripts/custom.js
+++ src/scripts/custom.js
@@ -3,8 +3,6 @@ Initialize ASU React Components.
 Includes header, footer, cookie consent.
 */
 
-console.log(udsHeaderVars);
-
 window.addEventListener("DOMContentLoaded", event => {
 	AsuHeaderFooter.initGlobalHeader({
 		targetSelector: "#header-container",
@@ -22,7 +20,6 @@ window.addEventListener("DOMContentLoaded", event => {
 			partnerLogo: udsHeaderVars.partnerLogo,
 			animateTitle: udsHeaderVars.animateTitle,
 			expandOnHover: udsHeaderVars.expandOnHover,
-			// expandOnHover: false,
 			parentOrg: udsHeaderVars.parentOrg,
 			parentOrgUrl: udsHeaderVars.parentOrgUrl,
 			breakpoint: udsHeaderVars.breakpoint,
